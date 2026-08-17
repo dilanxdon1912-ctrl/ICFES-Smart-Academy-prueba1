33a34a2071df8f3c9b1e76235375e398b19b45d9
dilanxdon1912-ctrl <dilanxdon1912@gmail.com>
2026-08-17 16:04:28 -0500

    Configuracion final de base y nojekyll

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/ICFES-Smart-Academy-prueba1/',
 })
